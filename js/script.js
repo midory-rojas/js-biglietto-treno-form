2d5341f48f9309d464fe5d0b75d922ab42d3f286
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-06 22:45:48 +0100

    feat:Form finito

---
+++ js/script.js
@@ -0,0 +1,63 @@
+// Prendo tutti gli elementi
+
+const form = document.getElementById("ticket-form");
+const nameImput = document.getElementById("name");
+const kmImput = document.getElementById("km");
+const ageSelect = document.getElementById("age");
+const ticketSection = document.getElementById("ticket-section");
+const passengerName = document.getElementById("passenger-name");
+const offer = document.getElementById("offer");
+const carrozza = document.getElementById("carrozza");
+const codice = document.getElementById("codice");
+const price = document.getElementById("price");
+
+//Dati a disposizione
+// const scontoPerMenori = 20; // questa variabile RIMMARRA SEMPRE COSI
+// onst scontoPerOver = 40; // questa variabile RIMMARRA SEMPRE COSI
+const prezzoPerKm = 0.21; 
+
+// Altri < 65 - 18 = nessuno sconto
+
+// Form --> Event listener
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const nome = nameImput.value.trim();
+  const km = parseFloat(kmImput.value);
+  const fasciaEta = ageSelect.value;
+  ticketSection.classList.remove("d-none");
+
+  let prezzoBiglietto = km * prezzoPerKm;
+  let tipoSconto = "Biglietto Standart";
+
+  // Appllico gli sconti
+
+  if (fasciaEta === "minorenne") {
+    prezzoBiglietto = prezzoBiglietto * 0.8;
+    tipoSconto = "Sconto Minorenni";
+  } else if (fasciaEta === "over65") {
+    prezzoBiglietto = prezzoBiglietto * 0.6;
+    tipoSconto ="Sconto Over 65";
+  }
+
+  // Numeri casuali per la carroza
+
+  const numeroCarrozza = Math.floor(Math.random() *10) + 1;
+  const codiceCP = Math.floor(Math.random() * 90000) + 10000;
+
+  passengerName.textContent = nome;
+  offer.textContent = tipoSconto;
+  carrozza.textContent = numeroCarrozza;
+  codice.textContent = codiceCP;
+  price.textContent = prezzoBiglietto.toFixed(2) + `€`;
+
+});
+
+const cancelBtn = document.getElementById("cancel");
+
+cancelBtn.addEventListener("click", function(){
+  nameImput.value = "";
+  kmImput.value = "";
+  ageSelect.value = "maggiorenne";
+  ticketSection.classList.add("d-none");
+});
